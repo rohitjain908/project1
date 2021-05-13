22c0fa01628daacb6d529119fcfb90de3222b5c0
Rohit <rrohitjee2018@gmail.com>
2021-05-13 23:56:35 +0530

    Playing with function and urls

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-  path("",views.index,name="index")
+  path("",views.index,name="index"),
+  path("<int:num>",views.number_plus_five,name="number_plus_five"),
+  path("rohit",views.rohit,name="rohit"),
+  path("atul",views.atul,name="atul"),
+  path("<str:name>",views.greet,name="greet"),
 ]
--- hello/views.py
+++ hello/views.py
@@ -3,5 +3,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello, World!")
+  return HttpResponse("Hello!")
+
+def rohit(request):
+  return HttpResponse("Hello, Rohit!")
+
+def atul(request):
+  return HttpResponse("Happy Birthday Atul!")
+
+def greet(request,name):
+  return HttpResponse(f"Hello, {name.capitalize()}!")
+
+def number_plus_five(request,num):
+  return HttpResponse(f"The Magical number is {num+5}")
+
+
 
