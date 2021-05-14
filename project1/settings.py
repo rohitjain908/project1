a0471c98b866e14c78a1ab0ea1dfb703742fa98c
Rohit <rrohitjee2018@gmail.com>
2021-05-15 00:43:09 +0530

    Completed newyear app

--- project1/settings.py
+++ project1/settings.py
@@ -115,11 +115,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
